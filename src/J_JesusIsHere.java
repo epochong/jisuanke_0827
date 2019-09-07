05190a8f17c20c871b1eb495bea23c68a34b2ad4
epochong <Valentineone@example.com>
2019-09-07 15:28:10 +0800

    网络赛

---
+++ src/J_JesusIsHere.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author epochong
+ * @date 2019/8/27 14:52
+ * @email epochong@163.com
+ * @blog epochong.github.io
+ * @describe
+ */
+public class J_JesusIsHere {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        List <Integer> list = new ArrayList <>();
+        for (int i = 0; i < n; i++) {
+            list.add(input.nextInt());
+        }
+        List <String> strings = new ArrayList <>();
+        strings.add("");
+        strings.add("c");
+        strings.add("ff");
+    }
+}
